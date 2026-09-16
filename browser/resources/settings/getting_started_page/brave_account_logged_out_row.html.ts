e82f7d7b9103deaf1715a6b516acb367d4fc0a76
Claudio DeSouza <cdesouza@brave.com>
2026-09-16 23:26:28 +0100

    [cr156][wip] Lit ternary branches must be tagged template literals
    New `@webui-eslint/lit-element-template-structure` check requires that
    once either branch of a ternary is `html`...``, the other branch must
    be too (or `nothing`/empty).
    
    Chromium changes:
    https://chromium.googlesource.com/chromium/src/+/621aaee3b0ae07c29137c248896b8f8df6be5684
    
    commit 621aaee3b0ae07c29137c248896b8f8df6be5684
    Author: rbpotter <rbpotter@chromium.org>
    Date:   Tue Sep 15 22:17:47 2026 -0700
    
        WebUI: Require ternary branches to be tagged template literals or empty
    
        This is to reduce complexity of auto formatting code by limiting the
        number of possible cases. It also makes the logical flow of the
        template more clear, because nested ternaries are logically nested
        if statements and not switch/case statements, so the template behind
        each nested ternary is indented under that ternary.
    
        Also fixing a few newly added violations, that were added while the
        rule was under development.
    
        Bug: 512911634
        Change-Id: Ib2b884eead41a71e86531b18d610ba0f03ee6ee7
        Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8347292
        Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
        Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
        Cr-Commit-Position: refs/heads/main@{#1698643}

--- browser/resources/settings/getting_started_page/brave_account_logged_out_row.html.ts
+++ browser/resources/settings/getting_started_page/brave_account_logged_out_row.html.ts
@@ -26,8 +26,8 @@ export function getHtml(this: BraveAccountLoggedOutRowElement) {
             ${this.state.verification.intent
                 === LoggedOutVerificationIntent.kResetPassword
                 && this.state.verification.verifiedEmail
-              ? loadTimeData.getString(BraveAccountSettingsStrings
-                  .SETTINGS_BRAVE_ACCOUNT_RESET_PASSWORD_VERIFIED_ROW_DESCRIPTION)
+              ? html`${loadTimeData.getString(BraveAccountSettingsStrings
+                  .SETTINGS_BRAVE_ACCOUNT_RESET_PASSWORD_VERIFIED_ROW_DESCRIPTION)}`
               : html`${this.getVerificationDescription().beforeLink}<leo-link
                     @click=${this.onResendConfirmationEmailLinkClicked}
                     >${this.getVerificationDescription().linkLabel}</leo-link>${
