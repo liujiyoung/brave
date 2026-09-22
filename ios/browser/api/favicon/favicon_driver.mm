e9f7313a18ed579eebfa7a19117a33fbc16bbd6a
Claudio DeSouza <cdesouza@brave.com>
2026-09-22 19:28:49 +0100

    fixup! [cr156][ios] Update signature for ExtractFaviconURL

--- ios/browser/api/favicon/favicon_driver.mm
+++ ios/browser/api/favicon/favicon_driver.mm
@@ -152,8 +152,8 @@ void FaviconDriverObserver::OnFaviconUpdated(
 
   const GURL message_request_url = message.request_url().value();
 
-  std::vector<web::FaviconURL> urls =
-    web::ExtractFaviconURL(message.legacy_body()->GetList(), message_request_url);
+  std::vector<web::FaviconURL> urls = web::ExtractFaviconURL(
+      message.legacy_body()->GetList(), message_request_url);
   if (urls.empty()) {
     return;
   }
